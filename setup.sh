7cd4702e2b19fdcbd75ce4b55910318da99da47b
Arya Shah <arya.shah82@nmims.edu.in>
2021-10-22 11:52:44 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
 mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"arya.shah82@nmims.edu.in\"\n\
+" > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
+enableCORS=false\n\
 port = $PORT\n\
-enableCORS = false\n\
-\n\
 " > ~/.streamlit/config.toml
