426237ac0640ae75107ba6ce85a8e1a7cc384ee4
Arya Shah <arya.shah82@nmims.edu.in>
2021-10-22 09:47:42 +0530

    Add Authen 3 PHP

---
+++ authen3.php
@@ -0,0 +1,43 @@
+<?php  
+  include('connection.php');   
+    $username=$_POST['Uname'];  
+    $password=$_POST['Pass'];  
+
+      if(isset($_POST['reg'])){
+    $username = stripcslashes($username);  
+    $password = stripcslashes($password);  
+    $username = mysqli_real_escape_string($con, $username);  
+    $password = mysqli_real_escape_string($con, $password);  
+    if(!empty($_POST['Uname']) && !empty($_POST['Pass'])) 
+    {
+    	$sql = "select * from mydatabase.customer where username = '$username'";  
+    	$result = mysqli_query($con, $sql);  
+    	$row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+    	$count = mysqli_num_rows($result);  
+    	if($count >= 1){ 
+          echo '<script>alert("Username is already taken, Please try again with a different username!")</script>';
+        }  
+         else{ 
+             $query="INSERT INTO mydatabase.customer(username,password1) VALUES('$username','$password')";  
+              $result=mysqli_query($con,$query);  
+              if($result){  
+               echo "Account Successfully Created"; 
+               header("location: login.html"); 
+               } 
+               else {  
+                   echo "Failure!";  
+                     }  
+              }}
+           else
+           { 
+              echo '<script>alert("Fields are left empty! Try again.")</script>';
+            }
+
+}
+
+
+
+
+
+
+?>  
